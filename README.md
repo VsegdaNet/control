3a2cc4aeb9685c35ebe17743476f12966d36272a
Anna <anna@MacBook-Pro-Anna.local>
2023-02-28 09:59:42 -0800

    “Task”

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 ["hello", "2", "world", ":-)"] -> ["2", ":-)"]; 
 ["1234", "1567", "-2", "computer science"] -> ["-2"]; 
 ["Russia", "Denmark", "Kazan"] -> [].
+--- 
---
+++ Test_Control/Program.cs
@@ -0,0 +1,17 @@
+﻿
+string[] arr1 = new string[6] {"YOU", "task", "home", "93939", "=", "+"};
+string[] arr2 = new string[arr1.Length];
+
+void ArrayNum(string[] arrOne, string[] arrTwo);
+{
+    int count = 0;
+    for (int i = 0; i < arrOne.Length; i++)
+    {
+        if(arrOne[i].Length <= 3)
+        {
+            arrTwo[count] = arrOne[i];
+            count ++;
+        }
+
+    }
+}
